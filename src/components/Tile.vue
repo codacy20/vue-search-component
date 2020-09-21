3b2e0d91eedd425eb9da191681078c1160ac39ba
Amir Kiumarsi <codacy20@users.noreply.github.com>
2020-09-21 18:21:34 +0200

    tile and title issue

--- src/components/Tile.vue
+++ src/components/Tile.vue
@@ -3,7 +3,8 @@
     <a v-bind:href="item.links.html" target="_blank">
       <img v-bind:src="item.urls.small" alt="#">
     </a>
-    <span id="title">{{item.alt_description}}</span>
+    <span id="no-desc" v-if="!item.alt_description">sorry, didn't find a description for this photo</span>
+    <span id="title" v-if="item.alt_description">{{item.alt_description}}</span>
     <a v-bind:href="item.user.links.html" target="_blank" id="handle">
       <span id="sub-title">by: @{{item.user.name}} on Unsplash</span>
     </a>
@@ -14,16 +15,14 @@
 export default {
   name: "Tile",
   props: ["item"],
-  created() {
-    // console.log(this.item);
-  }
+  created() {}
 };
 </script>
 
 <style>
 #tile-container {
   display: flex;
-  justify-content: space-evenly;
+  justify-content: space-between;
   align-items: center;
   flex-direction: column;
   margin-right: 48px;
@@ -42,6 +41,7 @@ img {
   font-weight: 500;
   font-size: 24px;
   line-height: 30px;
+  height: 75px;
 }
 #sub-title {
   width: 100%;
@@ -49,5 +49,14 @@ img {
   font-size: 16px;
   line-height: 20px;
   color: #999FAA;
+}
+
+#no-desc {
+  color: #999FAA;
+  width: 100%;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 30px;
+  height: 75px;
 }
 </style>
