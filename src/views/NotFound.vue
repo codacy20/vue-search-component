07ad608870b61757693d76a519ae6776795919fb
codacy20 <kmuc72@gmail.com>
2020-12-20 17:13:14 +0100

    initial unit test

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,10 +1,14 @@
 <template>
-  <p>Page not found</p>
+  <div>
+    <p>Page not found</p>
+    <p>{{ msg }}</p>
+  </div>
 </template>
 
 <script>
 export default {
   name: "NotFound",
+  props: ["msg"],
   components: {},
 };
 </script>
