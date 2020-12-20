4774f537e00b978789f211f1fcd881a89d3737af
codacy20 <kmuc72@gmail.com>
2020-12-20 10:05:48 +0100

    styling of movie detail page

--- src/components/Movie/MovieContext.vue
+++ src/components/Movie/MovieContext.vue
@@ -1,19 +1,24 @@
 <template>
   <div class="card-container">
-    <span>{{ $route.params.id }}</span>
-    <div
-      class="image"
-      v-if="poster"
-      :style="{
-        backgroundImage: `url('https://image.tmdb.org/t/p/w200${poster}')`,
-      }"
-    ></div>
-    <span>{{ title }}</span>
-    <span>{{ tagline }}</span>
-    <span>{{ rating }}</span>
-    <span>{{ budget }}</span>
-    <span>{{ overview }}</span>
-    <span>{{ status }}</span>
+    <div class="image-container">
+      <div
+        class="image"
+        v-if="poster"
+        :style="{
+          backgroundImage: `url('https://image.tmdb.org/t/p/w200${poster}')`,
+        }"
+      ></div>
+    </div>
+    <div class="content-container">
+      <div class="content-inner-container">
+        <span id="title">{{ title }}</span>
+        <span v-if="tagline" id="sub-title">"{{ tagline }}"</span>
+        <span>{{ overview }}</span>
+        <span>Rating: {{ rating }}</span>
+        <span v-if="budget">Budget: $ {{ budget }}</span>
+        <span>Status: {{ status }}</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -55,12 +60,40 @@ style <style lang="scss" scoped>
   max-height: 39rem;
   border-radius: 20px;
   background: white;
-  .image {
-    width: 13rem;
-    max-width: 13rem;
-    min-height: 18rem;
-    object-fit: cover;
-    border-radius: 12px;
+  display: flex;
+  .image-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 35%;
+    .image {
+      width: 13rem;
+      max-width: 13rem;
+      min-height: 18rem;
+      object-fit: cover;
+      border-radius: 12px;
+    }
+  }
+  .content-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 65%;
+    justify-content: center;
+    padding: 3rem 3rem 3rem 0;
+    .content-inner-container {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      height: 75%;
+      #title {
+        font-size: 35px;
+      }
+      #sub-title {
+        font-size: 16px;
+        color: #999faa;
+      }
+    }
   }
 }
 </style>
